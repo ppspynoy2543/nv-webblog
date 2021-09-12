1fb915da93190d9047bbd568173d1cfe16213754
ppspynoy2543 <sapphayap62@gmail.com>
2021-09-13 00:32:01 +0700

    13 sep 2021

--- cilent/src/App.vue
+++ cilent/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <back-header />
     <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
@@ -16,8 +17,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
+  text-align: auto;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px auto auto auto;
+  line-height: 40px;
+  font-size:18px;
 }
 </style>
--- cilent/src/router/index.js
+++ cilent/src/router/index.js
@@ -2,14 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 //import HelloWorld from '@/components/HelloWorld'
 
-import UserIndex from '@/components/Index'
-import UserCreate from '@/components/CreateUser'
-import UserEdit from '@/components/EditUser'
-import UserShow from '@/components/ShowUser'
+import UserIndex from '@/components/Users/Index'
+import UserCreate from '@/components/Users/CreateUser'
+import UserEdit from '@/components/Users/EditUser'
+import UserShow from '@/components/Users/ShowUser'
+import Login from '@/components/login'
+
+import CommentIndex from '@/components/Comments/Index'
+//Blog
+import BlogIndex from '@/components/Blogs/Index'
+import BlogCreate from '@/components/Blogs/CreateBlog'
+import BlogEdit from '@/components/Blogs/EditBlog'
+import BlogShow from '@/components/Blogs/ShowBlog'
+
+//Authen
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/users',
@@ -30,6 +41,39 @@ export default new Router({
       path: '/user/:userId',
       name: 'user',
       component: UserShow
-    }
+    },
+    //Authem
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/blogs',
+      name: 'blogs',
+      component: BlogIndex
+    },
+    {
+      path: '/blogs/create',
+      name: 'blogs-edit',
+      component: BlogCreate
+    },
+    {
+      path: '/blog/edit/:blodId',
+      name: 'blog-edit',
+      component: BlogEdit
+    },
+    {
+      path: '/blog/:blogId',
+      name: 'blog',
+      component: BlogShow
+    },
+
+    //comment
+    {
+      path: '/comments',
+      name: 'comments',
+      component: CommentIndex
+    },
   ]
 })
